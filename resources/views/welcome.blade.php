8616441712e4248ba1820dd08cbe21833d2d87d6
Francisco Ros <fjros@moss.sh>
2019-12-11 16:57:15 +0100

    Update welcome template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laraveles.com
                 </div>
 
                 <div class="links">
